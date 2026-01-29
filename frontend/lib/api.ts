3d5669cd00f0e3c86fbe0986afee4c49fd0fd20f
Aayush Rajthala <48017699+AayushRajthala99@users.noreply.github.com>
2026-01-29 01:52:56 +0100

    Updated getAPIUrl method in frontend

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,3 +1,7 @@
 export const getApiUrl = () => {
-  return `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}`;
+  if (typeof window === "undefined") {
+    return `${process.env.NEXT_PUBLIC_API_URL}:${process.env.NEXT_PUBLIC_API_PORT}`;
+  }
+
+  return `${window.location.protocol}//${window.location.hostname}:${process.env.NEXT_PUBLIC_API_PORT}`;
 };
