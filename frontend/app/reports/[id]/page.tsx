90e0f0433cc61ced41c4955982073592b837ddbf
robinpats666 <patelrabin89@gmail.com>
2026-02-03 16:13:41 +0100

    fixed async issue on retrieving param from parent to get id for individual pages

--- frontend/app/reports/[id]/page.tsx
+++ frontend/app/reports/[id]/page.tsx
@@ -2,6 +2,5 @@ import ReportDetailClient from "./ReportDetailClient"
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
-  // console.log("Rendering Report Detail Page for ID:", id)
   return <ReportDetailClient id={id} />
 }
